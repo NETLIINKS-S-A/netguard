3f353a148483ef92bcee71d204b292f0cad353d1
Poll Castillo <alejandrocordovafree@gmail.com>
2023-01-06 00:22:56 -0500

    UPDATE: clear old files

--- public/scripts/Source/Complements/RequestOptions.js
+++
@@ -1,9 +0,0 @@
-import { UI } from "../Tools/AppElements.js";
-let requestHeader = new Headers();
-requestHeader.append("Authorization", `Bearer ${UI.accessToken}`);
-requestHeader.append("Cookie", "JSESSIONID=CDD208A868EAABD1F523BB6F3C8946AF");
-export let DTROptions = {
-    method: 'GET',
-    headers: requestHeader,
-    redirect: 'follow'
-};
