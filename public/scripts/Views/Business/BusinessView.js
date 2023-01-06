e54512f8f04cd3d42d73bc884d229b2f3e576f16
Poll Castillo <alejandrocordovafree@gmail.com>
2023-01-06 00:34:30 -0500

    UPDATE: data and filter
    Load data on tables and filter
    Pending: render pagination

--- public/scripts/Views/Business/BusinessView.js
+++ public/scripts/Views/Business/BusinessView.js
@@ -1,4 +1,5 @@
 import { UI } from "../../DomElements.js";
+import { DTROptions } from "../../RequestOptions.js";
 export async function renderBusiness() {
     const url = "https://backend.netliinks.com:443/rest/entities/Business?fetchPlan=full";
     const appContent = UI.App?.content;
@@ -17,4 +18,83 @@ export async function renderBusiness() {
 
             </tbody>
         </table>`;
+    const toolbox = UI.App?.tools;
+    toolbox.innerHTML = `
+        <div class="toolbox">
+            <button class="btn btn_icon"><i class="fa-solid fa-arrow-rotate-right"></i></button>
+            <button class="btn btn_icon"><i class="fa-solid fa-plus"></i></button>
+            <div class="toolbox_spotlight">
+                <input type="text" class="input input_spotlight" placeholder="Buscar por nombre" id="spotlight">
+                <label class="btn btn_icon spotlight_label" for="spotlight"><i class="fa-solid fa-filter"></i></label>
+            </div>
+        </div>`;
+    let searchData = [];
+    async function getData() {
+        const response = await fetch(url, DTROptions);
+        return await response.json();
+    }
+    const search = document.querySelector("#spotlight");
+    const tableBody = document.querySelector("#tableBody");
+    search?.addEventListener("keyup", () => {
+        const filteredDatas = searchData.filter(filteredData => `${filteredData.name}`.includes(search.value));
+        let filteredDataResult = filteredDatas.length;
+        displayFilteredItems(filteredDatas, tableBody, filteredDataResult, currentPage);
+        setupPagination(filteredDatas, pagination, UI.tableRows);
+    });
+    tableBody.innerHTML = `
+        <tr>
+            <td>Cargando...</td>
+            <td>Cargando...</td>
+            <td>Cargando...</td>
+            <td>Cargando...</td>
+        </tr>
+
+        <tr>
+            <td>Cargando...</td>
+            <td>Cargando...</td>
+            <td>Cargando...</td>
+            <td>Cargando...</td>
+        </tr>
+
+        <tr>
+            <td>Cargando...</td>
+            <td>Cargando...</td>
+            <td>Cargando...</td>
+            <td>Cargando...</td>
+        </tr>
+    `;
+    const data = await getData();
+    searchData = data;
+    // pagination
+    const pagination = document.getElementById("paginationCounter");
+    let currentPage = 1;
+    function displayFilteredItems(items, wrapper, rowsPerPage, page) {
+        wrapper.innerHTML = "";
+        page--;
+        let start = rowsPerPage * page;
+        let end = start + rowsPerPage;
+        let paginatedItems = items.slice(start, end);
+        for (let i = 0; i < paginatedItems.length; i++) {
+            let item = paginatedItems[i];
+            let itemElement = document.createElement("tr");
+            itemElement.innerHTML = `
+                    <tr>
+                        <td>${item.name}</td>
+                        <td>${item.id}</td>
+                        <td>${item.name}</td>
+                        <td>
+                            <button class="btn btn_table" onclick="editBusiness()">
+                                <i class="fa-solid fa-pencil"></i>
+                            </button>
+                        </td>
+                    </tr>
+                `;
+            wrapper.appendChild(itemElement);
+        }
+    } // End displayFilteredItems
+    function setupPagination(items, wrapper, rowsPerPage) {
+        wrapper.innerHTML = "";
+        let pageCount;
+    }
+    displayFilteredItems(searchData, tableBody, UI.tableRows, currentPage);
 }
