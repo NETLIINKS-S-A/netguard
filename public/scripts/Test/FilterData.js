94f1bc5d11cfe7cda9e0a9166fa42b9d769ef44e
Poll Castillo <alejandrocordovafree@gmail.com>
2023-02-11 22:43:54 -0500

    Add tasks for Nova

--- public/scripts/Test/FilterData.js
+++ public/scripts/Test/FilterData.js
@@ -1,5 +1,15 @@
 import { getEntitiesData } from "../Backend/Connection.js";
+import { AppContent } from "../Shared/Settings/Misc.settings.js";
 export async function filterData() {
     console.log("Starting test: Filter Data...");
+    AppContent.innerHTML = `
+    <h1 class="app_title">Filter data</h1>
+    <div class="container">
+        <div id="content"></div>
+    </div>
+    `;
     const GETDATA = await getEntitiesData("User");
+    const content = document.getElementById("content");
+    content.innerHTML = "Put your content here.";
+    console.log(GETDATA);
 }
--- source/TypeScript/Test/FilterData.ts
+++ source/TypeScript/Test/FilterData.ts
@@ -1,9 +1,23 @@
 import { getEntitiesData } from "../Backend/Connection.js"
 import { NLData } from "../Shared/Libs/lib.types.g.js"
+import { AppContent } from "../Shared/Settings/Misc.settings.js"
 
 export async function filterData(): Promise<void> {
     console.log("Starting test: Filter Data...")
 
+    AppContent.innerHTML = `
+    <h1 class="app_title">Filter data</h1>
+    <div class="container">
+        <div id="content"></div>
+    </div>
+    `
     const GETDATA: NLData = await getEntitiesData("User")
+    
+    const content =
+        <HTMLElement>document.getElementById("content")
+    
+    content.innerHTML = "Put your content here."
+    
+    console.log(GETDATA)
 
 }
